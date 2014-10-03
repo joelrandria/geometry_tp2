8e79f49d8fc0b986af3e1a94e95ed77620447945
jo <jo@jo-linux.home>
2014-10-03 09:10:10 +0200

    Réinitialisation

--- triangle2f.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRIANGLE2F_H
-#define TRIANGLE2F_H
-
-#include "vertex2f.h"
-
-vertex2f** get_random_triangle2f(int x_max, int y_max);
-int locate_vertex_triangle2f(vertex2f** triangle, vertex2f* p);
-
-#endif
--- vertex2f_ring.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VERTEX2F_RING_H__
-#define __VERTEX2F_RING_H__
-
-#include "vertex2f.h"
-
-void print_vertex2f_ring(vertex2f* ring, int direction);
-vertex2f* insert_vertex2f_in_ring(vertex2f* ring, vertex2f* vertex, int position);
-
-#endif
